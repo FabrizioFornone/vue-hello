0d41915e9855e5b7cf09fba12bee43d65871eb17
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-13 15:23:54 +0100

    Exercise Done + Bonus 1

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,12 @@
 /* Stampare a schermo un messaggio all’interno di un h1,
  utilizzando i data. */
 
+// Bonus 1: Inserire una immagine
+
 new Vue({
   el: "#app",
   data: {
     message: "Hello World!",
+    image: "https://www.memecreator.org/static/images/memes/5213460.jpg",
   },
 });
